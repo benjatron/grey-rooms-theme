3c9de0c7a0c64c3e80e39a842c67bc92da4483d6
Ben Davidson <ben@bendavidson.me>
2019-10-14 17:05:21 -0400

    Updated initialization

--- functions.php
+++ functions.php
@@ -1,12 +1,17 @@
 <?php
 /**
- * This file calls the app.php file.
+ * This file calls the vendor files and init.php file.
  *
- * All the fun stuff is in the app.php file and its imports. This is just here
+ * All the fun stuff is in the init.php file and its imports. This is just here
  * because WordPress requires a functions.php file.
  *
  * You can add code below, but the structure of this project allows for better
  * placement of files outside of the root directory. If you're unfamiliar with
  * where something should go, poke around!
  */
-require_once( dirname( __FILE__ ) . '/app/app.php' );
+
+// Composer files
+require_once __DIR__ . '/vendor/autoload.php';
+
+// FWD Boilerplate init file
+require_once __DIR__ . '/app/init.php';
