efa81565b3f403b5bc42df2769331feee862c0ed
Ben Davidson <ben@bendavidson.me>
2019-12-17 14:58:34 -0500

    Updated blank template

--- page-templates/_template-blank.php
+++ page-templates/_template-blank.php
@@ -2,17 +2,35 @@
 /**
  * Template Name: Blank Template
  */
-
 use FWD_Helper as FWD;
+
+$template = new Template_Index;
+$slug = $template->slug;
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
     <?php
     // Header meta, scripts, and styles
+    FWD::preload( $slug, 'css' );
+    FWD::preload( $slug, 'js' );
+
     FWD::the_layout( 'header' );
+
+    wp_enqueue_style( $slug );
     ?>
   </head>
   <body <?php body_class(); ?>>
+    <?php
+    // Body open meta and functions
+    FWD::the_layout( 'body-open' );
+    ?>
+    <div id="<?php echo $slug; ?>__wrapper">
+    </div>
+    <?php
+    // Footer meta and scripts
+    wp_enqueue_script( $slug );
+    FWD::the_component( null, 'meta/foot' );
+    ?>
   </body>
 </html>
