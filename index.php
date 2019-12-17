812378ab74911d6d2120f7ee45a4f0bba4d50850
Ben Davidson <ben@bendavidson.me>
2019-12-17 14:36:24 -0500

    Added script/style preloading, enqueing

--- index.php
+++ index.php
@@ -9,7 +9,12 @@ $slug = $template->slug;
   <head>
     <?php
     // Header meta, scripts, and styles
+    FWD::preload( $slug, 'css' );
+    FWD::preload( $slug, 'js' );
+
     FWD::the_layout( 'header' );
+
+    wp_enqueue_style( $slug );
     ?>
   </head>
   <body <?php body_class(); ?>>
@@ -21,6 +26,7 @@ $slug = $template->slug;
     </div>
     <?php
     // Footer meta and scripts
+    wp_enqueue_script( $slug );
     FWD::the_component( null, 'meta/foot' );
     ?>
   </body>
