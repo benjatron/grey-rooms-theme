4305a9150391bbf042061ae245425a7d6ff2f7ab
Ben Davidson <ben@bendavidson.me>
2020-02-06 14:30:33 -0500

    Swapped out markup

--- index.php
+++ index.php
@@ -1,34 +1,3 @@
 <?php
-use FWD_Helper as FWD;
-
-$template = new Template_Index;
-$slug = $template->slug;
-$meta = $template->meta;
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <?php
-    // Header meta, scripts, and styles
-    FWD::preload( $slug, 'css' );
-    FWD::preload( $slug, 'js' );
-
-    FWD::the_layout( $meta, 'header' );
-
-    wp_enqueue_style( $slug );
-    ?>
-  </head>
-  <body <?php body_class( $slug ); ?>>
-    <?php
-    // Body open meta and functions
-    FWD::the_layout( $meta, 'body-open' );
-    ?>
-    <div id="<?php echo $slug; ?>__wrapper">
-    </div>
-    <?php
-    // Footer meta and scripts
-    wp_enqueue_script( $slug );
-    FWD::the_component( null, 'meta/foot' );
-    ?>
-  </body>
-</html>
+// Default the page to the 404 template
+FWD_Helper::the_template( '404' );
