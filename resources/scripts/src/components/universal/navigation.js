6734ace159d83ee271b826038d922f0cceaf121f
Ben Davidson <ben@bendavidson.me>
2020-03-11 19:09:29 -0400

    Updated element query

--- resources/scripts/src/components/universal/navigation.js
+++ resources/scripts/src/components/universal/navigation.js
@@ -7,7 +7,7 @@ import '../../vendor/mmenu';
 document.addEventListener(
   "DOMContentLoaded", () => {
     // Handle preloading of drawer menus
-    jQuery('.navigation__links').hide().removeClass('navigation__links--preload').show();
+    jQuery('.navigation__links--drawer').hide().removeClass('navigation__links--preload').show();
     jQuery('.navigation__nav').hide().removeClass('navigation__nav--preload').show();
 
     new Mmenu( '#drawer-nav', {
