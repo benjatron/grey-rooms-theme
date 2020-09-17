5c7bd499e1291d9c438c57d81a8f9c28912befc4
Ben Davidson <ben@designfwd.com>
2020-09-16 21:38:35 -0400

    Added base files

---
+++ resources/scripts/src/pages/archive-post.js
@@ -0,0 +1,15 @@
+// Scripts for the post archive template
+'use strict'
+
+/**
+ * Imports SCSS for Webpack parsing and sets strict mode
+ */
+import '../../../styles/src/pages/archive-post.scss';
+
+/*
+ * Component imports
+ */
+
+/*
+ * Page-specific scripting
+ */
