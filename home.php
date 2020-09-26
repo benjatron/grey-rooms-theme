e5042f9b708933964e70fd692f641202c174abf7
Ben Davidson <ben@designfwd.com>
2020-09-26 13:20:29 -0400

    Updated markup

--- home.php
+++ home.php
@@ -8,6 +8,7 @@ $navigation = $template->navigation;
 $banner = $template->banner;
 $intro = $template->intro;
 $archive = $template->archive;
+$cta = $template->cta;
 $footer = $template->footer;
 ?>
 <!doctype html>
@@ -35,8 +36,41 @@ $footer = $template->footer;
           FWD::the_component( $navigation, 'universal/navigation' );
           ?>
         </section>
+        <section class="<?= $slug ?>__banner">
+          <img class="<?= "{$slug}__background {$slug}__background--mobile lazyload lazyload--blurUp" ?>"
+            data-sizes="auto" alt="<?= $banner['mobile']['alt'] ?>"
+            src="<?= $banner['mobile']['sizes']['preload'] ?>"
+            data-srcset="<?= FWD::the_srcset( $banner['mobile'], 1024 ) ?>"
+          />
+          <img class="<?= "{$slug}__background {$slug}__background--desktop lazyload lazyload--blurUp" ?>"
+            data-sizes="auto" alt="<?= $banner['desktop']['alt'] ?>"
+            src="<?= $banner['desktop']['sizes']['preload'] ?>"
+            data-srcset="<?= FWD::the_srcset( $banner['desktop'] ) ?>"
+          />
+        </section>
       </header>
       <main class="<?= $slug ?>__main">
+        <section class="<?= $slug ?>__intro">
+          <h1 class="<?= $slug ?>__headline">
+            <?= $intro['heading'] ?>
+          </h1>
+          <?= $intro['content'] ?>
+          <img class="<?= $slug ?>__signature lazyload lazyload--blurUp"
+            alt="<?= $intro['signature']['alt'] ?>" data-sizes="auto"
+            src="<?= $intro['signature']['sizes']['preload'] ?>"
+            data-srcset="<?= FWD::the_srcset( $intro['signature'],  ) ?>"
+          />
+        </section>
+        <section class="<?= $slug ?>__blog-roll">
+          <?php
+          FWD::the_component( $archive, 'content/blog-roll' );
+          ?>
+        </section>
+        <section class="<?= $slug; ?>__cta">
+          <?php
+          FWD::the_component( $cta, 'universal/cta' );
+          ?>
+        </section>
       </main>
       <footer class="<?= $slug; ?>__footer">
         <?php
