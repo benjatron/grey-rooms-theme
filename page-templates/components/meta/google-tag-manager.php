914af41d6c18ce2166884c325f1ba39d018397ff
Ben Davidson <ben@designfwd.com>
2020-10-29 22:57:26 -0400

    Updated Analytics code

--- page-templates/components/meta/google-tag-manager.php
+++ page-templates/components/meta/google-tag-manager.php
@@ -7,11 +7,14 @@ if( true === $gtm['active'] ):
   $id = $gtm['id'];
 ?>
   <link rel="preconnect" href="https://www.googletagmanager.com/" crossorigin>
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','<?= $id ?>');</script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $id ?>"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
+    gtag('config', '<?= $id ?>');
+  </script>
 <?php
 endif;
